0b64aa2eb22fe7b90bc461739f75670ae8eda528
Zapca <97467936+Zapca@users.noreply.github.com>
2025-03-09 09:36:51 +0530

    Initial commit - uploading all project files

--- git-upload.bat
+++ git-upload.bat
@@ -3,4 +3,4 @@
 "C:\Program Files\Git\cmd\git.exe" remote add origin https://github.com/Zapca/Logithon-25-Compliance-checker.git
 "C:\Program Files\Git\cmd\git.exe" add .
 "C:\Program Files\Git\cmd\git.exe" commit -m "Initial commit"
-"C:\Program Files\Git\cmd\git.exe" push -u origin master
+"C:\Program Files\Git\cmd\git.exe" push -u origin main
